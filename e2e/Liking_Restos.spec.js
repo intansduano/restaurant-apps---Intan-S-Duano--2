37f8dd7a1ac08be05db2a4a2e036c020679bb66f
Intan Duano <intan.duano2710@gmail.com>
2023-05-22 12:27:13 +0800

    e2e testing

---
+++ e2e/Liking_Restos.spec.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+const assert = require('assert');
+const { async } = require('regenerator-runtime');
+
+Feature('Liking Resto');
+
+Before(({ I }) => {
+    I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked resto', ({ I }) => {
+    I.seeElement('#query');
+    I.see('Tidak ada restaurant untuk ditampilkan', '.post-item__not__found');
+});
+
+Scenario('liking one resto', ({ I }) => {
+    I.see('Tidak ada restaurant untuk ditampilkan', '.post-item__not__found');
+
+    I.amOnPage('/');
+    // … kita akan mengisi uji coba berikutnya …
+});
+
+Scenario('liking one resto', async({ I }) => {
+    I.see('Tidak ada restaurant untuk ditampilkan', '.post-item__not__found');
+
+    I.amOnPage('/');
+
+    I.seeElement('.post-item__title a');
+
+    const firstRestaurant = locate('.post-item__title a').first();
+    const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+    I.click(firstRestaurant);
+
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+
+    I.amOnPage('/#/like');
+    I.seeElement('.post-item');
+
+    const likedRestaurantTitle = await I.grabTextFrom('.post-item__title');
+
+    assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+});
